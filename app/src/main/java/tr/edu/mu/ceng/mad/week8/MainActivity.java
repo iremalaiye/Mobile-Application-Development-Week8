f8f089c7b7e1f7a580e48084d22cd683fb9951c3
u210709077 <iremalaiye@posta.mu.edu.tr>
2024-12-05 18:50:18 +0300

    Initial commit

---
+++ app/src/main/java/tr/edu/mu/ceng/mad/week8/MainActivity.java
@@ -0,0 +1,238 @@
+package tr.edu.mu.ceng.mad.week8;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+    EditText txtUrl;
+    ImageView imgview;
+    Button btnDownload;
+    private static final int REQUEST_EXTERNAL_STORAGE=1;
+    private static String[] PERMISSIONS_STORAGE={
+            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy=new
+                StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
+        setContentView(R.layout.activity_main);
+        txtUrl=findViewById(R.id.txtURL);
+        btnDownload=findViewById(R.id.btnDownload);
+        imgview=findViewById(R.id.imgView);
+
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int permission =
+                        ActivityCompat.checkSelfPermission(MainActivity.this,
+                                android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (permission != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+                }
+
+                /*String fileName = "temp.jpg";
+                String imagePath =
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString()
+                                + "/" + fileName;
+                 downloadFile(txtUrl.getText().toString(), imagePath);
+
+                preview(imagePath);*/
+
+                else{
+              //  DownloadTask backgroundTask = new DownloadTask();
+              //  String[] urls = new String[1];
+               // urls[0] = txtUrl.getText().toString();
+               // backgroundTask.execute(urls);
+
+                Thread backgroundThread=
+                        new Thread(new DownloadRunnable(txtUrl.getText().toString()));
+                backgroundThread.start();
+            }
+
+            }
+        });
+
+    }
+
+
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length==2&&
+                grantResults[0]==PackageManager.PERMISSION_GRANTED
+                && grantResults[1]==PackageManager.PERMISSION_GRANTED){
+           /* String fileName = "temp.jpg";
+            String imagePath =
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString()
+                            + "/" + fileName;
+
+            downloadFile(txtUrl.getText().toString(), imagePath);
+
+            preview(imagePath);*/
+
+           DownloadTask backgroundTask=new DownloadTask();
+           String[] urls=new String[1];
+            urls[0]=txtUrl.getText().toString();
+            backgroundTask.execute(urls);
+
+
+
+        }
+        else{
+            Toast.makeText(this,
+                    "External storage permission is not granted",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void preview(String imagePath){
+        Bitmap image= BitmapFactory.decodeFile(imagePath);
+        float imageWidth=image.getWidth();
+        float imageHeight=image.getHeight();
+        int rescaledWidth=400;
+        int rescaledHeight=(int)(imageHeight*rescaledWidth/imageWidth);
+        Bitmap bitmap= Bitmap.createScaledBitmap(image,rescaledWidth,rescaledHeight,false);
+        imgview.setImageBitmap(bitmap);
+
+    }
+
+
+    private void downloadFile(String url, String imagePath) {
+        try {
+            URL strUrl = new URL(url);
+            URLConnection connection = strUrl.openConnection();
+            connection.connect();
+
+            InputStream inputStream = new BufferedInputStream(strUrl.openStream(), 8192);
+            OutputStream outputStream = new FileOutputStream(imagePath);
+
+            byte data[] = new byte[1024];
+            int count;
+            while ((count = inputStream.read(data)) != -1) {
+                outputStream.write(data, 0, count);
+            }
+
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+        private Bitmap rescaleBitmap(String imagePath){
+            Bitmap image= BitmapFactory.decodeFile(imagePath);
+            float imageWidth=image.getWidth();
+            float imageHeight=image.getHeight();
+            int rescaledWidth=400;
+            int rescaledHeight=(int)(imageHeight*rescaledWidth/imageWidth);
+            Bitmap bitmap= Bitmap.createScaledBitmap(image,rescaledWidth,rescaledHeight,false);
+            return bitmap;
+
+        }
+
+
+        class DownloadTask extends AsyncTask<String,Integer,Bitmap> {
+            ProgressDialog progressDialog;
+
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMax(100);
+                progressDialog.setIndeterminate(false);
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressDialog.setTitle("Downloading");
+                progressDialog.setMessage("Please wait..");
+                progressDialog.show();
+            }
+
+            @Override
+            protected Bitmap doInBackground(String... urls) {
+                String fileName = "temp.jpg";
+                String imagePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+                downloadFile(urls[0], imagePath + "/" + fileName);
+                return rescaleBitmap(imagePath + "/" + fileName);
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                imgview.setImageBitmap(bitmap);
+                progressDialog.dismiss();
+            }
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                progressDialog.setProgress(values[0]);
+            }
+
+        }
+
+        class DownloadRunnable implements Runnable{
+            String url;
+            public DownloadRunnable(String url){
+                this.url=url;
+            }
+            @Override
+            public void run() {
+                String fileName="temp.jpg";
+                String imagePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+                downloadFile(url, imagePath + "/" + fileName);
+                Bitmap bitmap= rescaleBitmap(imagePath+"/"+fileName);
+
+                runOnUiThread(new UpdateBitmap(bitmap));
+            }
+
+
+
+        class UpdateBitmap implements Runnable{
+            Bitmap bitmap;
+            public UpdateBitmap(Bitmap bitmap) {
+                this.bitmap = bitmap;
+            }
+            @Override
+            public void run() {
+                imgview.setImageBitmap(bitmap);
+            }
+
+
+        }
+
+
+
+    }
+}
+
